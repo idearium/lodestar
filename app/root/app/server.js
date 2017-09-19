c45f4475d26336f7a6c94dbd04aac51d0745b8e8
Scott Mebberson <scott@scottmebberson.com>
2017-09-20 08:44:33 +0930

    Updated the lodestar app to contain the star symbol.

--- app/root/app/server.js
+++ app/root/app/server.js
@@ -6,7 +6,13 @@ const { createServer } = require('http');
 const port = process.env.APP_PORT || 80;
 
 // Create a really simple response.
-createServer((req, res) => res.end(`Lodestar (${Math.random()})`))
+createServer((req, res) => {
+
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+
+    return res.end(`<p>Lodestar ✫ (${Math.random()})</p>`);
+
+})
     .listen(port, (err) => {
 
         if (err) {
